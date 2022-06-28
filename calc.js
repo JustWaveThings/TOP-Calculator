e7f930ad0018750c6280c36e40df378c3bed3c22
JustWaveThings <flove101@gmail.com>
2022-06-28 17:08:43 -0400

    Refine pseudocode. Start fleshing out functions

--- calc.js
+++ calc.js
@@ -40,6 +40,8 @@ const arrayMemory = [];
 const displayArray = []; 
 const displayValue = disp(displayArray);
 
+
+
 function disp(displayArray) {
   return document.getElementById('displayNums').textContent = displayArray;
 }
@@ -59,10 +61,31 @@ calcButtons.forEach((button) =>
 console.log(displayValue);
 
 
+// handle logic section 
+
+const arrayStatus = arrayMemory.length;
+
+function arrayHandler(arrStatus) {
+  if (arrStatus === 0) {
+    /*
+      - disable(or ignore) all inputs except numbers, and dot
+              - once dot has been clicked, and another number has been clicked, disable dot button until operator button hit
+            - if minus is hit display message "premium feature"
+    */
+  } else if (arrStatus === 1) {
+    /* 
+    - enable all inputs and handle per refactor logic above
+             - if the next to last element is a '/' and the last is '0' = display "Div by 0 Error" and disable all buttons except clear (maybe turn it a different color too)
+    */
+  }
+
+}
+
+
 /* 
 pseudocode for calc logic:
 
-basic use case (normal // expected  use)
+basic use case (normal // expected use)
 
     Storing / sending for evaluation / displaying / clearing memory.
 
@@ -80,7 +103,7 @@ basic use case (normal // expected  use)
         - based on this, it looks like we need to evaluate the array's last 3 elements and if it matches a pattern of operand - operator - operand, then we evaluate, append to array and display 
 
       for the refactored path we'd need to use:
-        - arrayMemory to hold user inputs and the returned evaluations 
+        - arrayMemory to hold user inputs and the returned evaluations  (done)
         - array method to append user inputs / evaluations to arrayMemory (push())
         - array method to evaluate last 3 elements of array (switch or generic conditional)
         - clear array function when user hits clear (splice() with a 100 element delete argument )
@@ -94,14 +117,18 @@ basic use case (normal // expected  use)
             - disable(or ignore) all inputs except numbers, and dot
               - once dot has been clicked, and another number has been clicked, disable dot button until operator button hit
             - if minus is hit display message "premium feature"
-            - if the next to last element is a '/' and the last is '0' = display "Div by 0 Error" and disable all buttons except clear (maybe turn it a different color too)
           at array[0]
             - enable all inputs and handle per refactor logic above
+             - if the next to last element is a '/' and the last is '0' = display "Div by 0 Error" and disable all buttons except clear (maybe turn it a different color too)
 
 
       - output
         - Round to 4 decimal places if needed. Don't put 0's on to pad.  (conditional that tests how many decimal places are in the returned evaluation and calls round(), else doesn't change value.)
         
+
+      - handling arrayMemory
+        - if statement to check length of array
+
      
      
       - evaluating  
@@ -127,13 +154,3 @@ let's get it done!!
 
 
 
-
-
-
-/* instructions 
-Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
-
-You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
-
-This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
-*/
