f8a8f3b4f7ea165a2044b3acde515527b79d203e
JustWaveThings <flove101@gmail.com>
2022-06-28 00:00:58 -0400

    Display values on calculator - work in progress

--- calc.js
+++ calc.js
@@ -28,3 +28,25 @@ function operate (num1, num2, operation) {
 }
 
 console.log(operate(2000, 2, multiply));
+
+
+/*
+Create the functions that populate the display when you click the number buttons… you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+*/
+
+const disp = Element.querySelector(displayNums);
+  
+result = "testing";
+
+const displayValue = (result) => (disp.textContent);
+
+displayValue();
+
+
+/*
+Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
+
+You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
+
+This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
+*/
