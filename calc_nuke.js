6791a636163e079c480b54599595ab1b1fea9037
JustWaveThings <flove101@gmail.com>
2022-08-04 21:21:46 -0400

    Flesh out all the calculator functions short of handling display digits

--- calc_nuke.js
+++ calc_nuke.js
@@ -11,53 +11,68 @@ Plan
     -subtract √
     -divide √
     -multiply √
-    -operate
-    -equal
+    -operate √
+    -equals √
     -clear √
-    -input decimal (bonus)
-    -divide by zero 
-    -backspace (bonus)
-    -early equals
+    -divide by zero error √
+    -backspace (bonus) √
+    -early equals √
 
 - other things needed
-    - array to hold current operand or operator 
-    - array to hold each completed number / operand for evaluation
-    - array which contain what is displayed on the calculator screen (implement last)
+    - array to hold current operand or operator √
+    - array to hold each completed number / operand for evaluation √
+    - array which contain what is displayed on the calculator screen (implement last) 
     
 */
+const memory = []; // need to hold completed numbers / operands for eval by calculate function
+const currentMemory = []; // need array for current 'uncommitted' number / operand
+const displayScreen = []; // need array for what will be seen by user on display
+
 
 function add(num1, num2) {
   return num1 + num2;
 }
+
 function subtract(num1, num2) {
   return num1 - num2;
 }
+
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
 function divide(num1, num2) {
   return num1 / num2;
 }
-function operate(num1, num2, operation) {
-  return operation(num1, num2);
+
+function operate(num1 = memory[memory.length - 3], num2 = memory[memory.length - 1], operation = memory[memory.length - 2]) {
+  if (isFinite(num1) && isFinite(num2) && (typeof(operation) === "string")) {
+    if (operation === '/' && num2 === 0) return console.log("divide by zero error");
+    if (operation === '+') return add(num1, num2);
+    if (operation === '-') return subtract(num1, num2);
+    if (operation === '*') return multiply(num1, num2);
+    if (operation === '/') return divide(num1, num2);
+  } else {
+    console.log("ERROR");
+  }
+  console.log("test");
 }
 
-const memory = []; // need to hold completed numbers / operands for eval by calculate function
-const currentMemory = []; // need array for current 'uncommitted' number / operand
-const displayScreen = []; // need array for what will be seen by user on display
-let lastAction = 0; // need a variable to hold the last keypress
-
-
+function equals(operator = memory[memory.length - 1]) {
+  if (operator === '=') return operate();
+}
 
 function clear() {
     memory.splice(0, memory.length);
     currentMemory.splice(0, currentMemory.length);
     displayScreen.splice(0, displayScreen.length);
-    console.log("Clear has been pushed");
+    console.log("CLEAR");
 }
 
-function backspace(currentMemory = currentMemory, displayScreen = displayScreen){
-    currentMemory.pop();
-    displayScreen.pop();
-    console.log("backspace has been pushed");
+function backspace(currentMem = currentMemory){
+    currentMem.pop();
+    //displayScreen.pop();
+    console.log("<<");
 }
+
+
